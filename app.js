64390b206140f08660817fcab699476943dd2904
Alperen <109556793+alpralperen@users.noreply.github.com>
2024-08-04 19:08:22 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+//define elements
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const searchBtn = document.getElementById("search-btn");
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+// event listener
+
+searchBtn.addEventListener("click", search);
+
+// functions
+
+function search() {
+  const inpWord = document.getElementById("inp-word").value;
+  fetch(url + inpWord)
+    .then((response) => response.json())
+    .then((data) => {
+      result.innerHTML = `
+      <div class="word">
+          <h3>${data[0].word}</h3>
+          <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
+        </div>
+        <div class="details">
+          <p>${data[0].meanings[0].partOfSpeech}</p>
+          <p>${data[0].phonetic}</p>
+        </div>
+        <p class="word-meaning">${data[0].meanings[0].definitions[0].definition}</p>
+        <p class="word-example">${data[0].meanings[0].definitions[0].example}</p>
+      `;
+      sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+    })
+    .catch(
+      () => (result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`)
+    );
+}
+
+function playSound() {
+  sound.play();
+}
